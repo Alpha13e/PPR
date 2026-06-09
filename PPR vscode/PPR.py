f27998033bdfcc582a34f2611f884302b54fdc4e
Alpha13e <quentinbuet2005@gmail.com>
2026-06-09 11:46:47 +0200

    parsec def & gestion erreur en cours

--- PPR vscode/PPR.py
+++ PPR vscode/PPR.py
@@ -1,32 +1,29 @@
 #%% Parallax
-# Opening and first line
-f=open("gaia-dr2-allwise-2mass-ppmxl.csv",'r')
-g=open("parallax_results.txt",'w')
+# Traitements préliminaires
+f=open("/home/alpha/Documents/PPR/PPR vscode/gaia-dr2-allwise-2mass-ppmxl.csv",'r')
+g=open("/home/alpha/Documents/PPR/PPR vscode/parallax_results.txt",'w')
 
 title=f.readline()
-print(title)
 title=title.strip('\n')
 titles=title.split(',')
 
-g.write(titles[0]+"\t"
-        +titles[1]+"(mas)\t"
-        +titles[2]
-        +"\tdistance(m)\tdistance(pc)\n") # un en trop ? 
+g.write("source_id\tparallax (mas)\tparallax_error(mas)\tdistance(pc)\tdistance_error (pc)\n")
 
-# Automation
+# Automatisation
 
 for i in range(100):
     l=f.readline()
-    id,par,err,*_=l.split(',')
+    id,par,perr,*_=l.split(',')
 
-    par=par+0.029 # erreur zero-point (-biais, biais=-0.029)
-    dist=str(2.02/(float(par)*1e-3)) # Conversion. checker l'unité ?
-    err=dist*abs(err/par) # propagation de l'erreur
+    par=float(par)+0.029 # erreur zero-point (-biais, biais=-0.029)
+    dist=1/(par*1e-3)
+    derr=dist*abs(float(perr)/par) # propagation de l'erreur
 
-    g.write(id+"\t"+par+"\t"+err+"\t"+dist+"\n")
+    g.write(f"{id}\t{par}\t{perr}\t{dist}\t{derr}\n")
 
 g.close()
 f.close()
+print("Done")
 
 """
 Gaia at Lagrange L2: 1.01 au from sun-> 149597870700 m = 1 au
